11df0abe171387eda77d0a93133d751d45ef3035
madonghao <madonghao>
2019-12-11 17:58:38 +0800

    fix: 商品管理系统 测试连接数据库和dubbo

--- gmall-pms/src/main/java/com/mdh/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/mdh/gmall/pms/GmallPmsApplication.java
@@ -1,13 +1,19 @@
 package com.mdh.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo
+@MapperScan(basePackages = "com.mdh.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GmallPmsApplication.class, args);
     }
+
+
 
 }
--- gmall-pms/src/test/java/com/mdh/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/mdh/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.mdh.gmall.pms;
 
+import com.alibaba.fastjson.JSONObject;
+import com.mdh.gmall.pms.entity.Product;
+import com.mdh.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    ProductService productService;
+
     @Test
     void contextLoads() {
+        Product product = productService.getById(1);
+        System.out.println(JSONObject.toJSONString(product));
     }
+
 
 }
