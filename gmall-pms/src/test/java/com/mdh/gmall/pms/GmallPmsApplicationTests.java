236a69059fb56e3083c923e07b770bdc704b1f78
madonghao <madonghao>
2019-12-12 18:01:07 +0800

    fix: 配置logback  本地没生成日志？待解决

--- gmall-pms/src/test/java/com/mdh/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/mdh/gmall/pms/GmallPmsApplicationTests.java
@@ -4,11 +4,15 @@ import com.alibaba.fastjson.JSONObject;
 import com.mdh.gmall.pms.entity.Product;
 import com.mdh.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmallPmsApplicationTests {
+    private final static Logger logger = LoggerFactory.getLogger(GmallPmsApplicationTests.class);
+
 
     @Autowired
     ProductService productService;
@@ -16,6 +20,7 @@ class GmallPmsApplicationTests {
     @Test
     void contextLoads() {
         Product product = productService.getById(1);
+        logger.info("ahaha");
         System.out.println(JSONObject.toJSONString(product));
     }
 
